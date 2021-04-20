4ae48babd45135bf079df845c1972f34dc14a6fd
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-20 13:50:38 +0300

    Корректировка index.html для публикации

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./components/Pages/Home";
+import Catalog from "./components/Pages/Catalog";
+import About from "./components/Pages/About";
+import Contacts from "./components/Pages/Contacts";
+import Header from "./components/Header";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/contacts.html" component={Contacts} />
+        <Route path="/about.html" component={About} />
+        <Route path="/catalog.html" component={Catalog} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
