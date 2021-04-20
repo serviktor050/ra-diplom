d311e15c46b88da7c6b7907486482554d1f8a590
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-20 16:09:19 +0300

    Добавлен html хедера

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,39 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Menu from "./Menu";
 
 export default function Header() {
   return (
     <header className="container">
-      <div class="row">
-        <Menu />
+      <div className="row">
+        <div className="col">
+          <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+              <img src="img/header-logo.png" alt="Bosa Noga" />
+            </Link>
+            <div className="collapase navbar-collapse" id="navbarMain">
+              <Menu />
+              <div>
+                <div className="header-controls-pics">
+                  <div
+                    data-id="search-expander"
+                    className="header-controls-pic header-controls-search"
+                  ></div>
+                  <div className="header-controls-pic header-controls-cart">
+                    <div className="header-controls-cart-full">1</div>
+                    <div className="header-controls-cart-menu"></div>
+                  </div>
+                </div>
+                <form
+                  data-id="search-form"
+                  className="header-controls-search-form form-inline invisible"
+                >
+                  <input className="form-control" placeholder="Поиск" />
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
