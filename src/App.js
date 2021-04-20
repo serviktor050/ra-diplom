4b04ee7232847fbf3c48b0eb67067923b8a43222
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-20 14:36:26 +0300

    Реализован роутинг страниц

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Home from "./components/Pages/Home";
 import Catalog from "./components/Pages/Catalog";
 import About from "./components/Pages/About";
 import Contacts from "./components/Pages/Contacts";
+import Cart from "./components/Pages/Cart";
+import PageNotFound from "./components/Pages/PageNotFound";
 import Header from "./components/Header";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
     <Router>
       <Header />
       <Switch>
+        <Route path="/404.html" component={PageNotFound} />
+        <Route path="/cart.html" component={Cart} />
         <Route path="/contacts.html" component={Contacts} />
         <Route path="/about.html" component={About} />
         <Route path="/catalog.html" component={Catalog} />
