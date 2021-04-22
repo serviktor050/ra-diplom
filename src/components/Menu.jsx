fbae8f8a0c45f127493cd3feeaf7025a893843c9
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-22 12:15:48 +0300

    Корректировка отображения активных ссылок

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,42 +5,22 @@ export default function Menu() {
   return (
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <NavLink
-          exact
-          className="nav-link"
-          activeClassName={"nav-link active"}
-          to="/"
-        >
+        <NavLink exact className="nav-link" to="/">
           Главная
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          exact
-          className="nav-link"
-          activeClassName={"nav-link active"}
-          to="/catalog.html"
-        >
+        <NavLink exact className="nav-link" to="/catalog.html">
           Каталог
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          exact
-          className="nav-link"
-          activeClassName={"nav-link active"}
-          to="/about.html"
-        >
+        <NavLink exact className="nav-link" to="/about.html">
           О магазине
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink
-          exact
-          className="nav-link"
-          activeClassName={"nav-link active"}
-          to="/contacts.html"
-        >
+        <NavLink exact className="nav-link" to="/contacts.html">
           Контакты
         </NavLink>
       </li>
