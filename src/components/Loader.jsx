769f6f9fcccd6446fccea78432db4a3a4d8bbf4d
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-26 19:26:19 +0300

    Добавлен компонент визуального отображения загрузки данных

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div className="preloader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
