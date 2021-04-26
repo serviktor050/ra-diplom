0b64408b1c5e3d838e42b306f3dc45bee27efb39
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-26 19:55:51 +0300

    Добавлен компонент для превью карточки товара

---
+++ src/components/ProductCardSmall.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ProductCardSmall(props) {
+  const { id, title, images, price } = props;
+  return (
+    <div className="col-4">
+      <div className="card">
+        <img src={images[0]} className="card-img-top img-fluid" alt={title} />
+        <div className="card-body">
+          <p className="card-text">{title}</p>
+          <p className="card-text">{price} руб.</p>
+          <Link to={`/products/${id}.html`} className="btn btn-outline-primary">
+            Заказать
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
