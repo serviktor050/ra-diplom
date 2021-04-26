21977d7193700f8ea3e7be17a1c578b6b399b560
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-26 11:47:25 +0300

    Добавлены компоненты Хиты продаж и Каталог (для вставки)

---
+++ src/components/CatalogComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function CatalogComponent() {
+  return (
+    <section classТфьу="catalog">
+      <h2 class="text-center">Каталог</h2>
+
+      <div class="preloader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/SalesHits.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SalesHits() {
+  return (
+    <section className="top-sales">
+      <h2 className="text-center">Хиты продаж!</h2>
+
+      <div className="preloader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+  );
+}
