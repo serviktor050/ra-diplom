43a647d02ad0c5dae84b9ab72d5fa8158e360ea2
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-27 15:47:15 +0300

    Реализован компонент Каталог для главной страницы

--- src/components/CatalogComponent.jsx
+++ src/components/CatalogComponent.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProductsListRequest } from "./http/actions/actionCreators";
+import Loader from "./Loader";
+import ProductCardCatalog from "./ProductCardCatalog";
+import CatalogFilter from "./CatalogFilter";
+import ButtonDownloadMore from "./ButtonDownloadMore";
 
 export default function CatalogComponent() {
-  return (
-    <section classТфьу="catalog">
-      <h2 class="text-center">Каталог</h2>
+  const { products, loadingCatalog, errorCatalog } = useSelector(
+    (state) => state.productsList
+  );
+  const dispatch = useDispatch();
 
-      <div class="preloader">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(fetchProductsListRequest());
+  }, []);
+
+  return (
+    <section className="catalog">
+      <h2 className="text-center">Каталог</h2>
+      <CatalogFilter />
+      <div className="row">
+        {loadingCatalog && <Loader />}
+        {!loadingCatalog && !errorCatalog && (
+          <>
+            {products.map((product) => {
+              return (
+                <ProductCardCatalog
+                  key={product.id}
+                  id={product.id}
+                  category={product.category}
+                  title={product.title}
+                  images={product.images}
+                  price={product.price}
+                />
+              );
+            })}
+          </>
+        )}
       </div>
+      <ButtonDownloadMore />
     </section>
   );
 }
