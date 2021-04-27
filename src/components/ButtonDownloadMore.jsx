546b2816c33f2dc368a61958bb118be6738d8bb7
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-27 16:09:05 +0300

    Добавлен компонент кнопки Загрузить еще

---
+++ src/components/ButtonDownloadMore.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ButtonDownloadMore() {
+  return (
+    <div className="text-center">
+      <button className="btn btn-outline-primary">Загрузить ещё</button>
+    </div>
+  );
+}
