c8afda7b12275af3e7a2490f1ebf9d3fca95f69d
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-27 15:48:54 +0300

    Созданы отдельные компоненты карточки товара для Каталога и Хитов продаж

--- src/components/ProductCardCatalog.jsx
+++ src/components/ProductCardCatalog.jsx
@@ -5,7 +5,7 @@ export default function ProductCardSmall(props) {
   const { id, title, images, price } = props;
   return (
     <div className="col-4">
-      <div className="card">
+      <div className="card catalog-item-card">
         <img src={images[0]} className="card-img-top img-fluid" alt={title} />
         <div className="card-body">
           <p className="card-text">{title}</p>
