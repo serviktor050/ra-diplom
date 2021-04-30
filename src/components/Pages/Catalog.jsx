ffae20de2002caf84d15fd957aaafb4f0f89be4a
serviktor050 <mr.s-gagulin@yandex.ru>
2021-04-30 17:35:12 +0300

    Корректировка компонентов на странице Каталог

--- src/components/Pages/Catalog.jsx
+++ src/components/Pages/Catalog.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchProductsListRequest } from "../http/actions/actionCreators";
-import Loader from "../Loader";
+import React from "react";
 import Banner from "../Banner";
-import ProductCardCatalog from "../ProductCardCatalog";
-import CatalogFilter from "../CatalogFilter";
-import ButtonDownloadMore from "../ButtonDownloadMore";
+import CatalogComponent from "../CatalogComponent";
+import SearchField from "../SearchField";
 
 export default function Catalog() {
-  const { products, loadingCatalog, errorCatalog } = useSelector(
-    (state) => state.productsList
-  );
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchProductsListRequest());
-  }, []);
-
   return (
     <main className="container">
       <div className="row">
@@ -24,30 +11,8 @@ export default function Catalog() {
           <Banner />
           <section className="catalog">
             <h2 className="text-center">Каталог</h2>
-            <form className="catalog-search-form form-inline">
-              <input className="form-control" placeholder="Поиск" />
-            </form>
-            <CatalogFilter />
-            <div className="row">
-              {loadingCatalog && <Loader />}
-              {!loadingCatalog && !errorCatalog && (
-                <>
-                  {products.map((product) => {
-                    return (
-                      <ProductCardCatalog
-                        key={product.id}
-                        id={product.id}
-                        category={product.category}
-                        title={product.title}
-                        images={product.images}
-                        price={product.price}
-                      />
-                    );
-                  })}
-                </>
-              )}
-            </div>
-            <ButtonDownloadMore />
+            <SearchField />
+            <CatalogComponent />
           </section>
         </div>
       </div>
