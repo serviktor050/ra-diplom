d5cdf1e0204b8f205bd1c60ce7399c33c1fec80e
serviktor050 <mr.s-gagulin@yandex.ru>
2021-05-12 16:17:00 +0300

    Добавлена обработка ошибки запроса для кнопки Загрузить еще

--- src/components/ButtonDownloadMore.jsx
+++ src/components/ButtonDownloadMore.jsx
@@ -6,13 +6,11 @@ import {
   fetchDownloadMoreSearchResultsRequest,
   fetchAllDownloadMoreSearchResultsRequest,
 } from "../redux/productsList/actions/actionsCreators";
+import Loader from "./Loader";
 
 export default function ButtonDownloadMore() {
-  const {
-    products,
-    /*loadingCatalog, errorCatalog,*/ category,
-    search,
-  } = useSelector((state) => state.productsList);
+  const { products, loadingCatalog, errorCatalog, category, search } =
+    useSelector((state) => state.productsList);
 
   let id = category;
   let length = products.length;
@@ -37,10 +35,23 @@ export default function ButtonDownloadMore() {
   };
 
   return (
-    <div className="text-center">
-      <button className="btn btn-outline-primary" onClick={downloadMore}>
-        Загрузить ещё
-      </button>
-    </div>
+    <>
+      {loadingCatalog && <Loader />}
+      {!loadingCatalog && errorCatalog && (
+        <div>
+          <p>
+            Произошла ошибка при загрузке данных. Вы можете повторить запрос.
+          </p>
+          <button onClick={downloadMore}>Повторить запрос</button>
+        </div>
+      )}
+      {!loadingCatalog && !errorCatalog && (
+        <div className="text-center">
+          <button className="btn btn-outline-primary" onClick={downloadMore}>
+            Загрузить ещё
+          </button>
+        </div>
+      )}
+    </>
   );
 }
